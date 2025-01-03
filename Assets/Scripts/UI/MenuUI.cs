c542cb31748ab81b32bcf88b84de5277a9f953a6
doshuraaak <skopichdasha123@gmail.com>
2025-01-03 20:27:33 +0100

    Join a host and exit menues

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -8,13 +8,37 @@ public class MenuUI : MonoBehaviour
     [Header("Panels")]
     [SerializeField]
     private GameObject _settingsPanel;
-    
+    [SerializeField]
+    private GameObject _exitPanel;
+    [SerializeField]
+    private GameObject _codePanel;
+
     [Header("Buttons")]
     [SerializeField]
     private Button _settingsButton;
+    [SerializeField]
+    private Button _settingsBackgroundButton;
+    [SerializeField]
+    private Button _exitButton;
+    [SerializeField]
+    private Button _exitBackgroundButton;
+    [SerializeField]
+    private Button _exitNoButton;
+    [SerializeField]
+    private Button _codeButton;
+    [SerializeField]
+    private Button _codeBackgroundButton;
+
+
 
     private void Start()
     {
         _settingsButton.onClick.AddListener(() => _settingsPanel.SetActive(!_settingsPanel.activeInHierarchy));
+        _settingsBackgroundButton.onClick.AddListener(() => _settingsPanel.SetActive(!_settingsPanel.activeInHierarchy));
+        _exitButton.onClick.AddListener(() => _exitPanel.SetActive(!_exitPanel.activeInHierarchy));
+        _exitBackgroundButton.onClick.AddListener(() => _exitPanel.SetActive(!_exitPanel.activeInHierarchy));
+        _exitNoButton.onClick.AddListener(() => _exitPanel.SetActive(!_exitPanel.activeInHierarchy));
+        _codeButton.onClick.AddListener(() => _codePanel.SetActive(!_codePanel.activeInHierarchy));
+        _codeBackgroundButton.onClick.AddListener(() => _codePanel.SetActive(!_codePanel.activeInHierarchy));
     }
 }
